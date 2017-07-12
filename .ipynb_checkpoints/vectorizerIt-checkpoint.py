df673cbae2a9af143ff8eeb04e7bb51726753125
lorrin <zjuwangjue@gmail.com>
2017-07-12 04:38:39 -0400

    update

--- .ipynb_checkpoints/vectorizerIt-checkpoint.py
+++ .ipynb_checkpoints/vectorizerIt-checkpoint.py
@@ -1,10 +1,67 @@
 
 import numpy as np
-from sklearn.feature_extraction.text import TfidfTransformer  
-from sklearn.feature_extraction.text import CountVectorizer 
+import scipy.sparse as sp
+from sklearn.feature_extraction.text import *
+from sklearn.feature_extraction.text import _document_frequency
+from sklearn.utils.validation import check_is_fitted
+from sklearn.preprocessing import normalize
 from collections import namedtuple
 import gensim, logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+class DfidfTransformer(TfidfTransformer):
+    
+    def fit(self, X, y=None):
+        if not sp.issparse(X):
+            X = sp.csc_matrix(X)
+        if self.use_idf:
+            n_samples, n_features = X.shape
+            df = _document_frequency(X)
+            
+            # perform idf smoothing if required
+            #df += int(self.smooth_idf)
+            #n_samples += int(self.smooth_idf)
+
+            # log+1 instead of log makes sure terms with zero idf don't get
+            # suppressed entirely.
+            idf = np.log(1 + 1/df)
+            
+            self._df_diag = sp.spdiags(df, diags=0, m=n_features,
+                                       n=n_features, format='csr')
+            self._idf_diag = sp.spdiags(idf, diags=0, m=n_features,
+                                        n=n_features, format='csr')
+        return self
+    
+    def transform(self, X, copy=True):
+        if hasattr(X, 'dtype') and np.issubdtype(X.dtype, np.float):
+            # preserve float family dtype
+            X = sp.csr_matrix(X, copy=copy)
+        else:
+            # convert counts or binary occurrences to floats
+            X = sp.csr_matrix(X, dtype=np.float64, copy=copy)
+
+        n_samples, n_features = X.shape
+
+        if self.sublinear_tf:
+            np.log(X.data, X.data)
+            X.data += 1
+
+        if self.use_idf:
+            check_is_fitted(self, '_idf_diag', 'idf vector is not fitted')
+            check_is_fitted(self, '_df_diag', 'idf vector is not fitted')
+
+            expected_n_features = self._idf_diag.shape[0]
+            if n_features != expected_n_features:
+                raise ValueError("Input has n_features=%d while the model"
+                                 " has been trained with n_features=%d" % (
+                                     n_features, expected_n_features))
+            # *= doesn't work
+            X = X * self._idf_diag * self._df_diag
+
+        if self.norm:
+            X = normalize(X, norm=self.norm, copy=False)
+
+        return X
 
 class TfIdf(object):
     
@@ -26,6 +83,35 @@ class TfIdf(object):
         #获取词袋模型中的所有词语
         words=vectorizer.get_feature_names()
         
+        self._transformer = transformer
+        self._vectorizer = vectorizer
+        self.vecs = vecs
+        self.words = words
+        
+    def vectorizer(self, post):
+        return self._vectorizer.transform([post])
+    
+class DfIdf(object):
+    
+    def __init__(self, corpus):
+        self.corpus = corpus
+        self.vecs = []
+        self.words = []
+        self._vectorizer = None
+        self.train()
+        
+    def train(self):
+        # tf-idf, word[j],tfidf[i, j]
+        #该类会将文本中的词语转换为词频矩阵，矩阵元素a[i][j] 表示j词在i类文本下的词频 
+        vectorizer=CountVectorizer() 
+        #该类会统计每个词语的tf-idf权值 
+        transformer=DfidfTransformer() 
+        #第一个fit_transform是计算tf-idf，第二个fit_transform是将文本转为词频矩阵  
+        vecs=transformer.fit_transform(vectorizer.fit_transform(self.corpus))
+        #获取词袋模型中的所有词语
+        words=vectorizer.get_feature_names()
+        
+        self._transformer = transformer
         self._vectorizer = vectorizer
         self.vecs = vecs
         self.words = words
